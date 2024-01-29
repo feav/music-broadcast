440682e4d472c84f7236c81c0670bf354f739989
Armel EMBOLO <feavfeav@gmail.com>
2024-01-29 15:56:30 +0100

    retirer les scripts inutiles

--- ansible/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install Docker and Docker Compose on the remote server if not already installed
-ssh root@85.215.237.144 'bash -s' < /path/on/remote/server/install-docker.sh
-
-# Run Ansible playbook
-ansible-playbook -i /path/on/remote/server/hosts /path/on/remote/server/deploy.yml
